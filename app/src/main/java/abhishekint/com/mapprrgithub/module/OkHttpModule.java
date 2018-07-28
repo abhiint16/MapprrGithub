16890114bc2dfd07a60833c5f6c4854eb544bcff
Abhishek Kumar <abhishekint16@gmail.com>
2018-07-28 11:00:31 +0530

    init OkHttpModule

--- app/src/main/java/abhishekint/com/mapprrgithub/module/OkHttpModule.java
+++ app/src/main/java/abhishekint/com/mapprrgithub/module/OkHttpModule.java
@@ -1,7 +1,21 @@
 package abhishekint.com.mapprrgithub.module;
 
+import java.util.concurrent.TimeUnit;
+
 import dagger.Module;
+import dagger.Provides;
+import okhttp3.OkHttpClient;
 
 @Module
 public class OkHttpModule {
+
+    @Provides
+    public OkHttpClient getOkHttp()
+    {
+        return new OkHttpClient.Builder()
+                .connectTimeout(30L, TimeUnit.SECONDS)
+                .readTimeout(30L, TimeUnit.SECONDS)
+                .retryOnConnectionFailure(true)
+                .build();
+    }
 }
