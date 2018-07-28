1a4fb2058d606630d872c7ab50d0c8cd2399d0e1
Abhishek Kumar <abhishekint16@gmail.com>
2018-07-28 10:52:26 +0530

    providing getter for application context

--- app/src/main/java/abhishekint/com/mapprrgithub/module/ApplicationContextModule.java
+++ app/src/main/java/abhishekint/com/mapprrgithub/module/ApplicationContextModule.java
@@ -3,6 +3,7 @@ package abhishekint.com.mapprrgithub.module;
 import android.content.Context;
 
 import dagger.Module;
+import dagger.Provides;
 
 @Module
 public class ApplicationContextModule {
@@ -12,4 +13,10 @@ public class ApplicationContextModule {
     public ApplicationContextModule(Context applicationContext) {
         this.applicationContext = applicationContext;
     }
+
+    @Provides
+    public Context getApplicationContext() {
+        return applicationContext;
+    }
+
 }
