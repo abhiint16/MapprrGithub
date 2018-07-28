d99cca057325ecc9e1855ced5115f004c8d885e7
Abhishek Kumar <abhishekint16@gmail.com>
2018-07-28 10:27:13 +0530

    adding dagger dependency and making application level class

---
+++ app/src/main/java/abhishekint/com/mapprrgithub/MapprrGitHubApplication.java
@@ -0,0 +1,14 @@
+package abhishekint.com.mapprrgithub;
+
+import android.app.Application;
+
+/**
+ * Created by abhishek on 14-03-2018.
+ */
+
+public class MapprrGitHubApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
