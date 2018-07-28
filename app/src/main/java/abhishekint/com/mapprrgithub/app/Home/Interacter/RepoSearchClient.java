9f60fe6b21f45e64187980840753318a25ccb594
Abhishek Kumar <abhishekint16@gmail.com>
2018-07-28 18:47:19 +0530

    reformatting Get

--- app/src/main/java/abhishekint/com/mapprrgithub/app/Home/Interacter/RepoSearchClient.java
+++ app/src/main/java/abhishekint/com/mapprrgithub/app/Home/Interacter/RepoSearchClient.java
@@ -6,7 +6,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface RepoSearchClient {
-    @GET
+    @GET("search/repositories")
     Observable<RepoSearchModel> getHomeRepoSearch(@Query("q") String search, @Query("page") int page,
                                                   @Query("per_page") int pageSize, @Query("sort") String sort,
                                                   @Query("order") String order);
