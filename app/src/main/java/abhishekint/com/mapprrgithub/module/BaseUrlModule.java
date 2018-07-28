1e3d4ab705361bb26978af5253d530cf7e05b67e
Abhishek Kumar <abhishekint16@gmail.com>
2018-07-28 10:57:47 +0530

    adding base url

--- app/src/main/java/abhishekint/com/mapprrgithub/module/BaseUrlModule.java
+++ app/src/main/java/abhishekint/com/mapprrgithub/module/BaseUrlModule.java
@@ -1,7 +1,19 @@
 package abhishekint.com.mapprrgithub.module;
 
+import android.content.Context;
+
+import javax.inject.Named;
+
+import abhishekint.com.mapprrgithub.R;
 import dagger.Module;
+import dagger.Provides;
 
 @Module
 public class BaseUrlModule {
+
+    @Provides
+    @Named("top_head_url")
+    public String getBaseUrlRepoSearch(Context applicationContext) {
+        return applicationContext.getString(R.string.base_url_repo_search);
+    }
 }
