570f3ce1e42d672a82ac28f0ae2fc943e78bbd8b
Abhishek Kumar <abhishekint16@gmail.com>
2018-07-29 14:02:42 +0530

    init contributors activity

---
+++ app/src/main/java/abhishekint/com/mapprrgithub/app/ContributorDetails/PresentationLayer/ContributorDetailsActivity.java
@@ -0,0 +1,15 @@
+package abhishekint.com.mapprrgithub.app.ContributorDetails.PresentationLayer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import abhishekint.com.mapprrgithub.R;
+
+public class ContributorDetailsActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repodetails);
+    }
+}
