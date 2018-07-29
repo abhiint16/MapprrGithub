54351b784d014f585b12dc92eafb130c9b3c698e
Abhishek Kumar <abhishekint16@gmail.com>
2018-07-29 13:55:13 +0530

    creating interface for adapter

---
+++ app/src/main/java/abhishekint/com/mapprrgithub/app/RepoDetails/Adapter/AdapterRepoDetailsViewLayer.java
@@ -0,0 +1,8 @@
+package abhishekint.com.mapprrgithub.app.RepoDetails.Adapter;
+
+import java.util.List;
+import abhishekint.com.mapprrgithub.app.RepoDetails.Model.ContributorListModel;
+
+public interface AdapterRepoDetailsViewLayer {
+    void updateRepoDetailContributorData(List<ContributorListModel> contributorListModel);
+}
